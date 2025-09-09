18a91ef1e7eed9b647a7d78a3c2f100e886eb187
Jarred Sumner <jarred@jarredsumner.com>
2025-09-08 17:25:49 -0700

    Fix configuration issue caused by transpiling code for a different target than it is being run in

--- packages/minifiers/minifiers/bun.ts
+++ packages/minifiers/minifiers/bun.ts
@@ -8,7 +8,30 @@ export default createMinifier(
 	'bun',
 	{
 		default: async ({ filePath }) => {
-			const minify = spawn(bunPath, ['build', '--no-bundle', '--minify', filePath]);
+			const minify = spawn(bunPath, [
+				'build', 
+				'--no-bundle',
+				
+				// When no target is specified, bun assumes you're building for
+				// the browser. When building for the browser, bun sets
+				// `process.browser` to true to match various bundlers. In the future,
+				// Bun may disable this behavior when `--no-bundle` is passed, but
+				// for now it does not.
+				//
+				// When running TypeScript transpiled for the browser inside of
+				// Node.js, TypeScript defaults to including \r\n when no "sys"
+				// object is provided, which happens when process.browser
+				// evaluates to true.
+				//
+				// Since the spirit of this benchmark is mostly targeting browsers,
+				// we should keep it as implicit --target=browser even though the code
+				// testing it is not a browser.
+				'--define=process.browser:undefined',
+
+				'--minify', 
+				filePath,
+			]);
+
 			const [error, minified] = await Promise.all([
 				collectStream(minify.stderr),
 				collectStream(minify.stdout),
